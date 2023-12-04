e967367f6423542ce375da4c14ac45ec0e03370c
Stevie Rossetti <rossetti.stephen@gmail.com>
2023-12-04 12:43:38 -0600

    Added comments to css and javascript for user to understand method

--- script.js
+++ script.js
@@ -1,83 +1,74 @@
-let currentDay = dayjs().format('dddd, MMMM D, YYYY');
-$('#currentDay').text(currentDay);
+// Added a window.on 'load' listener to only run the function when window is loaded (i.e., browser has rendered all HTML)//
+$(window).on('load', function () {
+    
+    // Added a variable for current day using Day.js and applied that variable as text in the #currentDay ID line of HTML// 
+    let currentDay = dayjs().format('dddd, MMMM D, YYYY');
+    $('#currentDay').text(currentDay);
 
-let currentHour = dayjs().format('HH');
-let currentHourInt = parseInt(currentHour);
+    // Created variable for the current hour to utilize for highlightling timeblock rows for past/present/future functions//
+    let currentHour = dayjs().format('HH');
+    let currentHourInt = parseInt(currentHour);
+    
+    // Added a click event listener to the saveBtn. Since all div elements (hour-9, hour-10, etc.) have the same saveBtn ID for the button element, I utilized "this" to pinpoint the applicable element.//
+    // Backtracked from "this" saveBtn ID using "sibling" to create a variable to add a value to the decription ID (i.e., added locally stored text to text area element and not button element)//
+    // Backtracked from "this" saveBtn ID using "parent" to create a variable to grab the applicable hour title to organize local storage data (i.e., added locally stored descriptor to know what data to pull. For example, Hour-9 will have Hour-9 text)//
+    $(".saveBtn").click(function (event) {
+      event.preventDefault();
+      let eventNote = $(this).siblings(".description").val();
+      let hour = $(this).parent().attr("id");
+      localStorage.setItem(hour, eventNote);
+    });
+  
+    // Render value stored in localStorage (after saveBtn is clicked) that has applicable ID (e.g. hour-9) and class tag from the matching tag in localStorage//
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-$(".saveBtn").click(function (event) {
-    event.preventDefault();
-    let eventNote = $(this).siblings(".description").val();
-    let hour = $(this).parent().attr("id");
-    localStorage.setItem(hour, eventNote);
-});
+    // Likely a better way to do this, but created a function that turns the courrent hour in Day.js to an integer and then applies a past/present/future css attributed to each HTML timeblock based on what hour it is//
+    // Since all timeblocks begin with 'future' css class, we just need to apply past/present changes// 
+    function highlightHours () {
+      if (currentHourInt < 9) {
+        } else if (currentHourInt < 10 && currentHourInt >= 9) {
+          $('#hour-9').addClass('present');
+        } else if (currentHourInt < 11 && currentHourInt >= 10) {
+          $('#hour-9').addClass('past');
+          $('#hour-10').addClass('present');
+        } else if (currentHourInt < 12 && currentHourInt >= 11) {
+          $('#hour-9,#hour-10').addClass('past');
+          $('#hour-11').addClass('present');
+        } else if (currentHourInt < 13 && currentHourInt >= 12) {
+          $('#hour-9,#hour-10,#hour-11').addClass('past');
+          $('#hour-12').addClass('present');
+        } else if (currentHourInt < 14 && currentHourInt >= 13) {
+          $('#hour-9,#hour-10,#hour-11,#hour-12').addClass('past');
+          $('#hour-13').addClass('present');
+        } else if (currentHourInt < 15 && currentHourInt >= 14) {
+          $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13').addClass('past');
+          $('#hour-14').addClass('present');
+        } else if (currentHourInt < 16 && currentHourInt >= 15) {
+          $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14').addClass('past');  
+          $('#hour-15').addClass('present');
+        } else if (currentHourInt < 17 && currentHourInt >= 16) {
+          $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14,#hour-15').addClass('past');           
+          $('#hour-16').addClass('present');
+        } else if (currentHourInt < 18 && currentHourInt >= 17) {
+          $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14,#hour-15,#hour-16').addClass('past');    
+          $('#hour-17').addClass('present');
+        } else if (currentHourInt >= 18) {
+          $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14,#hour-15,#hour-16,#hour-17').addClass('past');
+        }
+  };
+  
+  //Run the coloring scheme function//
+  highlightHours ();
 
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
   });
 
 
-function highlightHours () {
-    if (currentHourInt < 9) {
-      } else if (currentHourInt < 10 && currentHourInt >= 9) {
-        $('#hour-9').addClass('present');
-      } else if (currentHourInt < 11 && currentHourInt >= 10) {
-        $('#hour-9').addClass('past');
-        $('#hour-10').addClass('present');
-      } else if (currentHourInt < 12 && currentHourInt >= 11) {
-        $('#hour-9,#hour-10').addClass('past');
-        $('#hour-11').addClass('present');
-      } else if (currentHourInt < 13 && currentHourInt >= 12) {
-        $('#hour-9,#hour-10,#hour-11').addClass('past');
-        $('#hour-12').addClass('present');
-      } else if (currentHourInt < 14 && currentHourInt >= 13) {
-        $('#hour-9,#hour-10,#hour-11,#hour-12').addClass('past');
-        $('#hour-13').addClass('present');
-      } else if (currentHourInt < 15 && currentHourInt >= 14) {
-        $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13').addClass('past');
-        $('#hour-14').addClass('present');
-      } else if (currentHourInt < 16 && currentHourInt >= 15) {
-        $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14').addClass('past');  
-        $('#hour-15').addClass('present');
-      } else if (currentHourInt < 17 && currentHourInt >= 16) {
-        $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14,#hour-15').addClass('past');           
-        $('#hour-16').addClass('present');
-      } else if (currentHourInt < 18 && currentHourInt >= 17) {
-        $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14,#hour-15,#hour-16').addClass('past');    
-        $('#hour-17').addClass('present');
-      } else if (currentHourInt >= 18) {
-        $('#hour-9,#hour-10,#hour-11,#hour-12,#hour-13,#hour-14,#hour-15,#hour-16,#hour-17').addClass('past');
-      }
-};
 
-highlightHours ();
